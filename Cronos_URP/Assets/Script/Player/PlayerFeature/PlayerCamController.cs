80e933444c0cf91b226399e2e9a8355264b3aa0c
Leusin <leusin@naver.com>
2024-07-25 20:27:39 +0900

    refector: PlayerCam 관련으로 리펙토링

--- Cronos_URP/Assets/Script/Player/PlayerFeature/PlayerCamController.cs
+++ Cronos_URP/Assets/Script/Player/PlayerFeature/PlayerCamController.cs
@@ -2,24 +2,40 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class PlayerCamControler : MonoBehaviour
 {
-	CinemachineVirtualCamera PlayerCam;
-	CinemachineBrain cinemachineBrain; 
-	private void OnEnable()
-	{
-// 		CinemachineBrain cinemachineBrain = Camera.main.GetComponent<CinemachineBrain>();
-// 		if (cinemachineBrain != null)
-// 		{
-// 			cinemachineBrain.m_UpdateMethod = CinemachineBrain.UpdateMethod.LateUpdate;
-// 		}
+    // 싱글턴 객체 입니다. 
+    public static PlayerCamControler Instance
+    {
+        get
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
 
+            // 인스턴스가 없다면 계층 구조창에서 검색해서 가져옴.
+            instance = FindObjectOfType<PlayerCamControler>();
 
-	}
+            return instance;
+        }
+    }
 
-	void FixedUpdate()
-	{
+    protected static PlayerCamControler instance;
 
-	}
+    private CinemachineVirtualCamera _virtualCamera;
+    public CinemachineVirtualCamera VirtualCamera { get { return _virtualCamera; } }
+
+    private void Awake()
+    {
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Start()
+    {
+        _virtualCamera.Follow = Player.Instance.transform.Find("Orientation");
+        _virtualCamera.LookAt = Player.Instance.transform.Find("Orientation");
+    }
 }
