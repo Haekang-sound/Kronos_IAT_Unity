6e76df2a12ed8b03806e705a22fe0e2fdf7b08e1
OHK <60260523+Haekang-sound@users.noreply.github.com>
2024-08-06 19:49:11 +0900

    add 능력개방 추가중

--- Cronos_URP/Assets/Script/OHK/Player/AbilityUsageInfo.cs
+++ Cronos_URP/Assets/Script/OHK/Player/AbilityUsageInfo.cs
@@ -1,18 +1,24 @@
 public struct AbilityUsageInfo
 {
-	/// <summary>
 	/// 오해강
-	/// </summary>
 	//콤보 강화
-	//수치 증가
+	public bool EnforcedCombo 
+	{
+		get{ return PlayerStateMachine.GetInstance().Animator.GetBool("EnforcedCombo"); }
+		set { PlayerStateMachine.GetInstance().Animator.SetBool("EnforcedCombo", value); }
+	}
+	//수치 증가 -> 나중에
+
 	//회피 공격
+
+
 	//Com_Attack 사용 불가
 	//Nor_Attack 사용 불가
 	//경직 면역
-		
+
 	/// 민동휘
 	//Com_S_Attack 강 공격 강화
-	
+
 	/// 김주영
 	//Nor_S_Attack 강 공격 강화
 	//공격 스택
---
+++ Cronos_URP/Assets/Script/OHK/Player/StateMachine/Behaviour/DodgeAttackBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeAttackBehaviour : StateMachineBehaviour
+{
+	PlayerStateMachine stateMachine;
+	[SerializeField] float moveForce;
+
+	public float hitStopTime;
+	[Range(0.0f, 1.0f)] public float minFrame;
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		stateMachine = PlayerStateMachine.GetInstance();
+		stateMachine.SwitchState(new PlayerAttackState(stateMachine));
+
+		stateMachine.MoveForce = moveForce;
+		stateMachine.HitStop.hitStopTime = hitStopTime;
+		animator.ResetTrigger("Attack");
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	//{
+	//    
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	//{
+	//    
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove()
+	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	    // Implement code that processes and affects root motion
+	}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK()
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	//{
+	//    // Implement code that sets up animation IK (inverse kinematics)
+	//}
+}
