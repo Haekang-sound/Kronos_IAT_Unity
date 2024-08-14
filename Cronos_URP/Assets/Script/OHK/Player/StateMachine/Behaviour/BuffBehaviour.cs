0211383348ed8e73b061238eff7b54fc52c44938
OHK <60260523+Haekang-sound@users.noreply.github.com>
2024-08-14 16:53:53 +0900

    fix 일단수정

--- Cronos_URP/Assets/Script/OHK/Player/StateMachine/Behaviour/BuffBehaviour.cs
+++ Cronos_URP/Assets/Script/OHK/Player/StateMachine/Behaviour/BuffBehaviour.cs
@@ -61,10 +61,10 @@ public class BuffBehaviour : StateMachineBehaviour
 	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove()
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-	//{
-	//    // Implement code that processes and affects root motion
-	//}
+	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	    // Implement code that processes and affects root motion
+	}
 
 	// OnStateIK is called right after Animator.OnAnimatorIK()
 	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerBuffState.cs
+++ Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerBuffState.cs
@@ -6,6 +6,7 @@ public class PlayerBuffState : PlayerBaseState
 	public PlayerBuffState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 	public override void Enter()
 	{
+		stateMachine.Rigidbody.velocity = Vector3.zero;
 	}
 	public override void Tick()
 	{
