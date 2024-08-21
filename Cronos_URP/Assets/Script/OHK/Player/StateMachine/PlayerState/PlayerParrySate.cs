1c542d4950c3c40570323d8d7be455c8d626da7b
OHK <60260523+Haekang-sound@users.noreply.github.com>
2024-08-21 15:29:36 +0900

    fix 이동 수정

--- Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerParrySate.cs
+++ Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerParrySate.cs
@@ -6,6 +6,7 @@ public class PlayerParryState : PlayerBaseState
 {
 	public PlayerParryState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 	private readonly int attackHash = Animator.StringToHash("Attack");
+	Vector3 totalMove;
 	public override void Enter()
 	{
 		stateMachine.InputReader.onLAttackStart += Attack;
@@ -14,25 +15,29 @@ public class PlayerParryState : PlayerBaseState
 			stateMachine.transform.rotation = Quaternion.LookRotation(stateMachine.Velocity);
 		}
 	}
-	public override void Tick() { }
+	public override void Tick()
+	{
+		Vector3 rootMotion = stateMachine.Animator.deltaPosition;
+		rootMotion.y = 0;
+		totalMove += rootMotion;
+	}
 	public override void FixedTick()
 	{
-        Vector3 rootMotion = stateMachine.Animator.deltaPosition;
-        rootMotion.y = 0;
-        if (IsOnSlope())
-        {
-            stateMachine.Rigidbody.velocity = AdjustDirectionToSlope(rootMotion) * stateMachine.MoveForce;
-        }
-        else
-        {
-            stateMachine.Rigidbody.velocity = AdjustDirectionToSlope(rootMotion) * stateMachine.MoveForce;
+		if (IsOnSlope())
+		{
+			stateMachine.Rigidbody.velocity = AdjustDirectionToSlope(totalMove) * stateMachine.MoveForce;
+		}
+		else
+		{
+			stateMachine.Rigidbody.velocity = AdjustDirectionToSlope(totalMove) * stateMachine.MoveForce;
 
-        }
+		}
 
+		totalMove = Vector3.zero;
 	}
 	public override void LateTick() { }
 
-	public override void Exit() 
+	public override void Exit()
 	{
 		stateMachine.InputReader.onLAttackStart += Attack;
 	}
