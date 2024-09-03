e7e47ad4d39ec0bae3faf9069119f3eee6297e7c
DESKTOP-37BOMML\sunri <sunriver94@gmail.com>
2024-09-03 09:05:33 +0900

    feat 제작중

--- Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerDefenseState.cs
+++ Cronos_URP/Assets/Script/OHK/Player/StateMachine/PlayerState/PlayerDefenseState.cs
@@ -20,8 +20,11 @@ public class PlayerDefenceState : PlayerBaseState
 //             
 //         }
     }
-	public override void FixedTick(){}
-	public override void LateTick(){}
+	public override void FixedTick()
+    {
+        Float();
+    }
+    public override void LateTick(){}
 	public override void Exit()
     {
         stateMachine.Player.EndGuard();
