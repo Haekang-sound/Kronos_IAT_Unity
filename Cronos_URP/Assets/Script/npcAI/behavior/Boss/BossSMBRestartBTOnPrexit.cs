ecd91ed934380905f5ca50904025346adece7cde
Leusin <leusin@naver.com>
2024-10-15 10:37:27 +0900

    fix: 보스의 애니메이터, BT 시스템 보완

--- Cronos_URP/Assets/Script/npcAI/behavior/Boss/BossSMBRestartBTOnPrexit.cs
+++ Cronos_URP/Assets/Script/npcAI/behavior/Boss/BossSMBRestartBTOnPrexit.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BossSMBRestartBT : SceneLinkedSMB<BossBehavior>
+public class BossSMBRestartBTOnPrexit : SceneLinkedSMB<BossBehavior>
 {
     public override void OnSLStatePreExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
