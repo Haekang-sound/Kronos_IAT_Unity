5823c6af8eda23d8a5fe322b71e42c8c2c15e57f
Leusin <leusin@naver.com>
2024-10-21 18:09:09 +0900

    add: 보스 입장 이벤트 컷신 중 스페이스바를 누르면 스킵 되도록 구현

---
+++ Cronos_URP/Assets/Script/BossrEnterEventController.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
+using static Controls;
+
+public class BossrEnterEventController : MonoBehaviour
+{
+    public GameObject timeline;
+
+    private PlayableDirector _playerbleDirector;
+    private bool _sceneSkipped;
+    //-----
+
+    void Awake()
+    {
+        _playerbleDirector = timeline.GetComponent<PlayableDirector>();
+    }
+
+    void Start()
+    {
+        timeline.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Skip();
+        }
+    }
+
+    private void Skip()
+    {
+        if (_sceneSkipped == false)
+        {
+            _playerbleDirector.time = 11f;
+            _sceneSkipped = true;
+        }
+    }
+
+}
