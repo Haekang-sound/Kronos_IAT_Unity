a2428819124484404be368e33bb1d5b6ae047ecc
napseed <xortggg@gmail.com>
2024-10-31 14:11:08 +0900

    새 플레이어 프리팹 수정

--- Cronos_URP/Assets/Script/OHK/Player/PlayerFeature/Player.cs
+++ Cronos_URP/Assets/Script/OHK/Player/PlayerFeature/Player.cs
@@ -137,8 +137,8 @@ public class Player : MonoBehaviour, IMessageReceiver
 	public EffectManager effectManager;
 	public ImpulseCam impulseCam;
 	public GameObject playerSword;
-	public GameObject spcCubeL;
-	public GameObject spcCubeR;
+	//public GameObject spcCubeL;
+	//public GameObject spcCubeR;
 	public float spcDelay;
 	public PlayerStateMachine psm;
 	public BoxColliderAdjuster adjuster;
@@ -383,19 +383,19 @@ public class Player : MonoBehaviour, IMessageReceiver
 
 		TP = _damageable.currentHitPoints;
 
-		// ������ ������ spcť�긦 Ȱ��ȭ.
-		if (psm.Velocity.x != 0f || psm.Velocity.z != 0f)
-		{
-			StartCoroutine(ActivateSpcCubes(spcDelay));
-		}
-		else
-		{
-			if (spcCubeL)
-			{
-				spcCubeL.SetActive(false);
-				spcCubeR.SetActive(false);
-			}
-		}
+		//// ������ ������ spcť�긦 Ȱ��ȭ.
+		//if (psm.Velocity.x != 0f || psm.Velocity.z != 0f)
+		//{
+		//	StartCoroutine(ActivateSpcCubes(spcDelay));
+		//}
+		//else
+		//{
+		//	if (spcCubeL)
+		//	{
+		//		spcCubeL.SetActive(false);
+		//		spcCubeR.SetActive(false);
+		//	}
+		//}
 	}
 
 
@@ -461,8 +461,11 @@ public class Player : MonoBehaviour, IMessageReceiver
 		positionToDamager -= transform.up * Vector3.Dot(transform.up, positionToDamager);
 		transform.rotation = Quaternion.LookRotation(positionToDamager);
 
+		soundManager.PlaySFX("Player_Pain_Sound_SE", transform);
+
 		switch (damageMessage.damageType)
 		{
+
 			case DamageType.ATypeHit:
 				PlayerFSM.Animator.SetTrigger("damagedA");
 				break;
@@ -725,33 +728,82 @@ public class Player : MonoBehaviour, IMessageReceiver
 			soundManager.PlaySFX("Character_voice_SE", transform);
 	}
 
-	public void Shake()
+	public void SoundFoot1()
+	{
+        if (soundManager != null)
+            soundManager.PlaySFX("Player_Walk_1_Sound_SE", transform);
+    }
+
+	public void SoundFoot2()
+	{
+        if (soundManager != null)
+            soundManager.PlaySFX("Player_Walk_2_Sound_SE", transform);
+    }
+
+	// 기획에 전달하기 위해 플레이어 키프레임으로 호출되는 사운드 출력 함수들
+	public void ComSlash1()
+	{
+		if (soundManager != null)
+			soundManager.PlaySFX("Com_Attack_1_Sound_SE", transform);
+	}
+
+    public void ComSlash2()
+    {
+        if (soundManager != null)
+            soundManager.PlaySFX("Com_Attack_2_Sound_SE", transform);
+    }
+
+    public void ComSlash3()
+    {
+        if (soundManager != null)
+            soundManager.PlaySFX("Com_Attack_3_Sound_SE", transform);
+    }
+
+    public void ComSlash4()
+    {
+        if (soundManager != null)
+            soundManager.PlaySFX("Com_Attack_4_Sound_SE", transform);
+    }
+
+	public void NorSlash1()
+	{
+		if (soundManager != null)
+			soundManager.PlaySFX("Player_Swing_1_Sound_SE", transform);
+	}
+
+    public void NorSlash2()
+    {
+        if (soundManager != null)
+            soundManager.PlaySFX("Player_Swing_2_Sound_SE", transform);
+    }
+
+    public void Shake()
 	{
 		if (impulseCam != null)
 			impulseCam.Shake();
 	}
 
-	IEnumerator ActivateSpcCubes(float delay)
-	{
-		yield return new WaitForSeconds(delay);
-		if (psm.Velocity.x != 0f || psm.Velocity.z != 0f)
-		{
-			spcCubeL.SetActive(true);
-			spcCubeR.SetActive(true);
-		}
-	}
+	//IEnumerator ActivateSpcCubes(float delay)
+	//{
+	//	yield return new WaitForSeconds(delay);
+	//	if (psm.Velocity.x != 0f || psm.Velocity.z != 0f)
+	//	{
+	//		spcCubeL.SetActive(true);
+	//		spcCubeR.SetActive(true);
+	//	}
+	//}
 
-	// �÷��̾� spcCube�� Ȱ��ȭ - Ű�����ӿ��� �̺�Ʈ�� ȣ��
-	public void ActivateSCube()
-	{
-		spcCubeL.SetActive(true);
-		spcCubeR.SetActive(true);
-	}
+	//// �÷��̾� spcCube�� Ȱ��ȭ - Ű�����ӿ��� �̺�Ʈ�� ȣ��
+	//public void ActivateSCube()
+	//{
+	//	spcCubeL.SetActive(true);
+	//	spcCubeR.SetActive(true);
+	//}
 
-	public void DeactivateSCube()
-	{
+	//public void DeactivateSCube()
+	//{
 
-	}
+	//}
 
 	protected IEnumerator RespawnRoutine()
 	{
